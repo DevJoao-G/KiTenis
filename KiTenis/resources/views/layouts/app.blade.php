fc5c60a3ec8932f4d439096c4bc8511b70fc4b69
João Vitor <244514662+DevJoao-G@users.noreply.github.com>
2026-01-14 10:50:04 -0300

    create view Login, fix-layout Header.

--- KiTenis/resources/views/layouts/app.blade.php
+++ KiTenis/resources/views/layouts/app.blade.php
@@ -1,15 +1,30 @@
- <header>
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <title>@yield('title', 'KiTenis')</title>
+
+    <!-- Bootstrap Icons -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.css" rel="stylesheet">
+
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+</head>
+
+<body>
+
+   
+        <header>
     <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
       <div class="container-fluid">
-        <a class="navbar-brand" href="#">KiTenis</a>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
-          aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <a class="navbar-brand" href="{{ route('welcome') }}">KiTenis</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#mainNavbar"
+          aria-controls="mainNavbar" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
         </button>
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <div class="collapse navbar-collapse" id="mainNavbar">
           <ul class="navbar-nav me-auto mb-2 mb-lg-0">
             <li class="nav-item">
-              <a class="nav-link active" aria-current="page" href="#">Todos</a>
+              <a class="nav-link active" aria-current="page" href="#">Feminino</a>
             </li>
             <li class="nav-item">
               <a class="nav-link" href="#">Masculino</a>
@@ -32,7 +47,7 @@
 
 
           <form class="d-flex ">
-            <a href="#" class="text-white fs-4 me-3" title="Entrar">
+            <a href="{{ route('login') }}" class="text-white fs-4 me-3" title="Entrar">
               <i class="bi bi-person-circle"></i>
             </a>
             <input class="form-control me-2" type="search" placeholder="Procure aqui..." aria-label="Search">
@@ -42,3 +57,11 @@
       </div>
     </nav>
   </header>
+    
+
+    <main>
+        @yield('content')
+    </main>
+
+</body>
+</html>
