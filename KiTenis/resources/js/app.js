2d3b949db093d13db38cc3d02698251d355c3093
Joao <244514662+DevJoao-G@users.noreply.github.com>
2026-01-19 16:52:55 -0300

    Laravel 12 limpo instalado, sem autenticação ainda.

--- KiTenis/resources/js/app.js
+++ KiTenis/resources/js/app.js
@@ -1,12 +1,1 @@
 import './bootstrap';
-import axios from 'axios';
-import './account';
-import './auth';
-import './header';
-import { showToast } from './toast';
-
-// deixa disponível globalmente (opcional mas prático)
-window.showToast = showToast;
-
-window.axios = axios;
-window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
--- KiTenis/routes/web.php
+++ KiTenis/routes/web.php
@@ -3,10 +3,5 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('Home');
-})->name('Home');
-
-
-Route::get('/account', function () {
-    return view('auth.Account');
-})->name('Account');
+    return view('welcome');
+});
